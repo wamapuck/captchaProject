b09d56bc1bbc9ff998fc7e56cd3dc3ca0b999f2d
Jaeyoon <wamapuck4270@gmail.com>
2024-01-05 15:36:30 +0900

    무 중복 수정본

--- generrateCaptcha.py
+++ generrateCaptcha.py
@@ -3,20 +3,22 @@ import random
 
 image = ImageCaptcha(width=180, height=90) # 이미지 사이즈
 
-def captchaGenerator():                                 #이미지 생성 함수
-    captchaKey = str(random.randint(100000,1000000))    #이미지에 들어갈 숫자 범위 지정과 램덤으로 수 생성
+    
+the_amount  = int(input('Please enter the amount you want to : '))
 
-    image.generate(captchaKey)
 
-    captchaKeyName = captchaKey + '.png'
+captchaKey = random.sample(range(100000,1000000),the_amount)
+
+for unchangedStringCaptchaKey in captchaKey:
+    
+    changedStringCaptchaKey = str(unchangedStringCaptchaKey)
+    
+    image.generate(changedStringCaptchaKey)
+
+    captchaKeyName = changedStringCaptchaKey + '.png'
 
     img_path = 'captchaExamples' + '/' + captchaKeyName
 
-    image.write(captchaKey,img_path)
-    
-the_amount  = int(input('Please enter the amount you want to : '))
-    
-for i in range(the_amount):
-    captchaGenerator()
-    
+    image.write(changedStringCaptchaKey,img_path)
+
 print("captchaGenerator generated",the_amount,"images successfully")
